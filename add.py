3bed36860b7490caa739d8229386498fe2214d55
Bryant Ortega <kalicorose@gmail.com>
2023-09-19 03:00:22 -0700

    updated script in add.py

--- add.py
+++ add.py
@@ -1,5 +1,9 @@
-variable_name = int(input("Enter two number to be added."))
-a = 3
-b = 4
+#Prompt user to enter numbers for two variables
+a = int(input("Enter a first number to be added."))
+b = int(input("Enter a second number to be added."))
+
+#The two numbers are added and value is stored in C
 c = a + b
+
+#display the value of the two added numbers
 print(c)
