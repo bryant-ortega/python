6db2e06967e37bef0d155fd49765ad8d98abb545
Bryant Ortega <kalicorose@gmail.com>
2023-09-25 00:35:43 -0700

    completed task 1.3

---
+++ Exercise-1.3/Exercise_1.3.py
@@ -0,0 +1,45 @@
+recipes_list = []
+ingredients_list = []
+
+def take_recipe(name='No name', cooking_time='No cooking time', ingredients='No ingredients', recipe='No recipe'):
+  name = input("Enter your recipe name: ")
+  cooking_time = int(input("Enter your recipe's cooking time in minutes: "))
+  ingredients = input("Enter your recipe's ingredients: ").split(", ")
+
+  recipe = {
+    'Name': name, 
+    'Cooking_time': cooking_time, 
+    'Ingredients': ingredients
+    }
+  return recipe
+
+n = int(input("How many recipes would you like to enter?: "))
+
+for i in range(n):
+  recipe = take_recipe()
+  for ingredient in recipe['Ingredients']:
+    if ingredient not in ingredients_list:
+      ingredients_list.append(ingredient)
+  recipes_list.append(recipe)
+
+for recipe in recipes_list:
+  if recipe['Cooking_time'] < 10 and len(recipe['Ingredients']) < 4:
+    difficulty = 'Medium'
+  elif recipe['Cooking_time'] >= 10 and len(recipe['Ingredients']) < 4:
+    difficulty = 'Intermediate'
+  elif recipe['Cooking_time'] >= 10 and len(recipe['Ingredients']) >= 4:
+    difficulty = 'Hard'
+
+  print('===================================')
+  print('Recipe: ', recipe['Name'])
+  print('Cooking Time (min): ', recipe['Cooking_time'])
+  print('Ingredients: ')
+  for ingredient in recipe['Ingredients']:
+    print(ingredient)
+  print('Difficulty level: ', difficulty)
+  
+print("---------------------------------------------\nIngredients used across all recipes\n===================================")
+sorted_list = sorted(ingredients_list)
+
+for ingredient in sorted_list:
+    print(ingredient)
--- Exercise-1.3/age.py
+++
@@ -1,2 +0,0 @@
-age = int(input("Enter your age: "))
-print("Age between 18 and 35: " + str(18 < age < 35))
