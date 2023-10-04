27e5cf6823d9ba41945fa0ceacd1e491f246f996
Bryant Ortega <chase@monarca.systems>
2023-10-04 10:56:56 -0700

    added ipython

--- practice.py
+++ practice.py
@@ -7,4 +7,6 @@ class Date(object):
 first_moon_landing = Date(20, 7, 1969)
 
 print("Initial values in first_moon_landing")
-print(first_moon_landing    )
+print(first_moon_landing.day)
+print(first_moon_landing.month)
+print(first_moon_landing.year)
