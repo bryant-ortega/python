63ddf40950a400f4b99d38b02fe2e753c0815a2a
Bryant Ortega <kalicorose@gmail.com>
2023-10-06 17:01:04 -0700

    completed practice task 2

--- Exercise-1.5/shopping_list.py
+++ Exercise-1.5/shopping_list.py
@@ -5,7 +5,7 @@ class ShoppingList(object):
 
   # Method to add new items to self.shopping_list
   def add_item(self, item):
-    if item not in self.shopping_list:
+    if not item in self.shopping_list:
       self.shopping_list.append(item)
       print(f"{item} has been added to the {self.list_name} shopping list.")
     else:
@@ -13,20 +13,41 @@ class ShoppingList(object):
 
   # Method to remove an item from self.shopping_list.
   def remove_item(self, item):
-    if item in self.shopping_list:
+    try:
       self.shopping_list.remove(item)
       print(f"{item} has been removed from the {self.list_name} shopping list.")
-    else:
+    except:
       print(f"{item} is not found in the {self.list_name} shopping list.")
 
-# Method to print the contents of self.shopping_list.
+  # Method to view the contents of self.shopping_list.
   def view_list(self):
-    print()
-    print(self.list_name)
-    print("---------------")
+    print("\nItems in " + str(self.list_name) + '\n' + 30*'-')
     for item in self.shopping_list:
-      print(item)
-    print()
+      print(' - ' + str(item))
+
+  def merge_lists(self, obj):
+    # creating a name for our new, merged shopping list
+    merged_lists_name = 'Merged List - ' + str(self.list_name) + " + " + str(obj.list_name)
+
+    # creating an empty ShoppingList object
+    merged_lists_obj = ShoppingList(merged_lists_name)
+
+    # Adding the first shopping list's items to our new list
+    merged_lists_obj.shopping_list = self.shopping_list.copy()
+
+    # Adding the second shopping list's items to our new list -
+    # we're doing this so that there won't be any repeated items
+    # in the final list, if both source lists contain common
+    # items between each other
+    for item in obj.shopping_list:
+      if not item in merged_lists_obj.shopping_list:
+        merged_lists_obj.shopping_list.append(item)
+
+    # Returning our new, merged object    
+    return merged_lists_obj
+
+
+
 
 pet_store_list = ShoppingList("Pet Store Shopping List")
 pet_store_list.add_item("dog food")
@@ -35,10 +56,11 @@ pet_store_list.add_item("bowl")
 pet_store_list.add_item("collars")
 pet_store_list.add_item("flea collars")
 
-pet_store_list.view_list()
+grocery_store_list = ShoppingList("Grocery Store List")
 
-pet_store_list.remove_item("flea collars")
+for item in ['fruits' ,'vegetables', 'bowl', 'ice cream']:
+    grocery_store_list.add_item(item)
 
-pet_store_list.add_item("frisbee")
+merged_list = ShoppingList.merge_lists(pet_store_list, grocery_store_list)
 
-pet_store_list.view_list()
+merged_list.view_list()
--- practice.py
+++ practice.py
@@ -1,57 +1,86 @@
-class Date(object):
-  def __init__(self, day, month, year):
-    self.day = day
-    self.month = month
-    self.year = year
+# class Date(object):
+#   def __init__(self, day, month, year):
+#     self.day = day
+#     self.month = month
+#     self.year = year
 
-  def get_date(self):
-    output = str(self.day) + "/" + str(self.month) + "/" + str(self.year)
+#   def get_date(self):
+#     output = str(self.day) + "/" + str(self.month) + "/" + str(self.year)
+#     return output
+
+  # def set_date(self):
+  #   self.day = int(input("Enter the day of the month: "))
+  #   self.month = int(input("Enter the month: "))
+  #   self.year = int(input("Enter the year: "))
+
+  # def is_leap_year(self):
+  #   return self.year % 4 == 0
+
+  # def is_valid_date(self):
+  #   if not (type(self.day) == int and type(self.month) == int and type(self.year) == int):
+  #     return False
+
+  #   if self.year < 0:
+  #     return False
+
+  #   if self.month < 1 or self.month > 12:
+  #     return False
+    
+  #   last_dates = {
+  #     1: 31,
+  #     2: 29 if self.is_leap_year() else 28,
+  #     3: 31,
+  #     4: 30,
+  #     5: 31,
+  #     6: 30,
+  #     7: 31,
+  #     8: 31,
+  #     9: 30,
+  #     10: 31,
+  #     11: 30,
+  #     12: 31
+  #   }
+
+  #   if self.day < 1 or self.day > last_dates.get(self.month):
+  #     return False
+    
+  #   return True
+
+# date = Date(1, 1, 2001)
+
+# print(Date.get_date(date))
+
+
+class Height(object):
+  def __init__(self, feet, inches):
+    self.feet = feet
+    self.inches = inches
+  
+  def __str__(self):
+    output = str(self.feet) + " feet, " + str(self.inches) + " inches, "
     return output
+  
+  def __sub__(self, other):
+    # Converting both objects' heights into inches
+    height_A_inches = self.feet * 12 + self.inches
+    height_B_inches = other.feet * 12 + other.inches
 
-  def set_date(self):
-    self.day = int(input("Enter the day of the month: "))
-    self.month = int(input("Enter the month: "))
-    self.year = int(input("Enter the year: "))
+    # Adding them up
+    total_height_inches = height_A_inches - height_B_inches
 
-  def is_leap_year(self):
-    return self.year % 4 == 0
+    # Getting the output in feet
+    output_feet = total_height_inches // 12
 
-  def is_valid_date(self):
-    if not (type(self.day) == int and type(self.month) == int and type(self.year) == int):
-      return False
+    # Getting the output in inches
+    output_inches = total_height_inches - (output_feet * 12)
 
-    if self.year < 0:
-      return False
+    # Returning the final output as a new Height object
+    return Height(output_feet, output_inches)
 
-    if self.month < 1 or self.month > 12:
-      return False
     
-    last_dates = {
-      1: 31,
-      2: 29 if self.is_leap_year() else 28,
-      3: 31,
-      4: 30,
-      5: 31,
-      6: 30,
-      7: 31,
-      8: 31,
-      9: 30,
-      10: 31,
-      11: 30,
-      12: 31
-    }
 
-    if self.day < 1 or self.day > last_dates.get(self.month):
-      return False
-    
-    return True
+person_A_height = Height(5, 10)
+person_B_height = Height(3, 9)
+height_difference = person_A_height - person_B_height
 
-date1 = Date(29, 2, 2000)           # Valid since it's a leap year.
-date2 = Date(29, 2, 2001)           # Invalid since it's not a leap year.
-date3 = Date('abc', 'def', 'ghi')   # Invalid since we're feeding in irrelevant values.
-
-
-
-print(str(date1.get_date()) + ": " + str(date1.is_valid_date()))
-print(str(date2.get_date()) + ": " + str(date2.is_valid_date()))
-print(str(date3.get_date()) + ": " + str(date3.is_valid_date()))
+print("Total height:", height_difference)
