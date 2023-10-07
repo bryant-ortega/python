1a3f695f1a1be680b7de22e882742980c41f47dd
Bryant Ortega <kalicorose@gmail.com>
2023-10-06 20:29:58 -0700

    added practice task 3

--- practice.py
+++ practice.py
@@ -76,11 +76,21 @@ class Height(object):
 
     # Returning the final output as a new Height object
     return Height(output_feet, output_inches)
+  
+  def __gt__(self, other):
+    height_inches_A = self.feet * 12 + self.inches
+    height_inches_B = other.feet * 12 + other.inches
+    return height_inches_A > height_inches_B
+  
+  def __ge__(self, other):
+    height_inches_A = self.feet * 12 + self.inches
+    height_inches_B = other.feet * 12 + other.inches
+    return height_inches_A >= height_inches_B
+  
+  def __ne__(self, other):
+    height_inches_A = self.feet * 12 + self.inches
+    height_inches_B = other.feet * 12 + other.inches
+    return height_inches_A != height_inches_B
 
-    
 
-person_A_height = Height(5, 10)
-person_B_height = Height(3, 9)
-height_difference = person_A_height - person_B_height
-
-print("Total height:", height_difference)
+  
